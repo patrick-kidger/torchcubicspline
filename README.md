b7da579cc93b25f8a958687bb8189af901e8fd7f
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2020-06-07 14:15:34 +0100

    version 0.0.2

--- README.md
+++ README.md
@@ -5,26 +5,91 @@ Interpolating natural cubic splines using PyTorch. Includes support for:
 - Support for missing values (represent them as NaN)
 - Evaluating the first derivative of the spline
 
+## Installation
+
+```bash
+pip install git+https://github.com/patrick-kidger/torchcubicspline.git
+```
+
+## Example
+
+Simple example:
+```python
+import torch
+from torchcubicspline import(natural_cubic_spline_coeffs, 
+                             NaturalCubicSpline)
+
+length, channels = 7, 3
+t = torch.linspace(0, 1, length)
+x = torch.rand(length, channels)
+coeffs = natural_cubic_spline_coeffs(t, x)
+spline = NaturalCubicSpline(coeffs)
+point = torch.tensor(0.4)
+out = spline.evaluate(point)
+```
+
+With multiple batch and evaluation dimensions:
+```python
+import torch
+from torchcubicspline import(natural_cubic_spline_coeffs, 
+                             NaturalCubicSpline)
+
+t = torch.linspace(0, 1, 7)
+# (2, 1) are batch dimensions. 7 is the time dimension
+# (of the same length as t). 3 is the channel dimension.
+x = torch.rand(2, 1, 7, 3)
+coeffs = natural_cubic_spline_coeffs(t, x)
+# coeffs is a tuple of tensors
+
+# ...at this point you can save the coeffs, put them
+# through PyTorch's Datasets and DataLoaders, etc...
+
+spline = NaturalCubicSpline(coeffs)
+
+point = torch.tensor(0.4)
+# will be a tensor of shape (2, 1, 3), corresponding to
+# batch, batch, and channel dimensions
+out = spline.derivative(point)
+
+point = torch.tensor([[0.4, 0.5]])
+# will be a tensor of shape (2, 1, 1, 2, 3), corresponding to
+# batch, batch, time, time and channel dimensions
+out = spline.derivative(point)
+```
+
 ## Functionality
 
-Functionality is provided via the `NaturalCubicSpline` class. It takes an increasing sequence of times represented by a tensor `times` of shape `(length,)` and some corresponding observations `X` of shape `(..., length, channels)`, where `...` are batch dimensions, and each `(length, channels)` slice represents `length` points, each in Euclidean space of dimension `channels`.
+Functionality is provided via the `natural_cubic_spline_coeffs` function and `NaturalCubicSpline` class.
 
-Then calling `spline = NaturalCubicSpline(times, X)` produces an instance `spline` such that
+`natural_cubic_spline_coeffs` takes an increasing sequence of times represented by a tensor `t` of shape `(length,)` and some corresponding observations `x` of shape `(..., length, channels)`, where `...` are batch dimensions, and each `(length, channels)` slice represents a sequence of `length` points, each point with `channels` many values.
+
+Then calling
+```python
+coeffs = natural_cubic_spline_coeffs(t, x)
+spline = NaturalCubicSpline(coeffs)
 ```
-spline.evaluate(times[i]) == X[..., i, :]
+produces an instance `spline` such that
+```
+spline.evaluate(t[i]) == x[..., i, :]
 ```
 for all `i`.
 
-Furthermore the derivative of the spline at a point may be calculated via `spline.derivative`. (Not be confused with backpropagation, which is also supported through both `spline.evaluate` and `spline.derivative`.)
+#### Why is there a function and a class?
 
-Finally, support for missing values is done by passing them as `NaN`. In particular this allows for batching elements with different observation times: take `times` to be the observation times of all elements in the batch, and just set each element to have a missing observation `NaN` at the times of the observations of the other batch elements.
+The slow bit is done during `natural_cubic_spline_coeffs`. The fast bit is `NaturalCubicSpline`. The returned `coeffs` are a tuple of PyTorch tensors, so you can take this opportunity to save or load them, push them through `torch.utils.data.Dataset` or `torch.utils.data.DataLoader`, etc.
+
+#### Derivatives
+
+The derivative of the spline at a point may be calculated via `spline.derivative`. (Not be confused with backpropagation, which is also supported through both `spline.evaluate` and `spline.derivative`.)
+
+#### Missing values
+
+Support for missing values is done by setting that element of `x` to `NaN`. In particular this allows for batching elements with different observation times: take `times` to be the observation times of all elements in the batch, and just set each element to have a missing observation `NaN` at the times of the observations of the other batch elements.
 
 ## Limitations
 
-If possible, you should cache the instance `spline` created by the call `spline = NaturalCubicSpline(times, X)`. This is because whilst `spline.evaluate` and `spline.derivative` should be quick to evaluate, some of the initial computations during `__init__` aren't parallelised as much as perhaps they could be. In general slow down is expected when:
-- `length` is large and the batch sizes are small (due to non-parallelism within `misc.tridiagonal_solve`).
-- or if the batch sizes are large and you have missing data (due to non-parallelism when working over every batch element to figure out where the missing values are).
+If possible, you should cache the coefficients returned by `natural_cubic_spline_coeffs`. In particular if there are missing values then the computation can be quite slow.
 
 ## Any issues?
 
-Any issues - let me know!
+Any issues or questions - open an issue to let me know. :)
---
+++ test/test_interpolate.py
@@ -0,0 +1,134 @@
+import torch
+import torchcubicspline
+
+
+# Represents a random natural cubic spline with a single knot in the middle
+class _Cubic:
+    def __init__(self, batch_dims, num_channels, start, end):
+        self.a = torch.randn(*batch_dims, 1, num_channels, dtype=torch.float64) * 10
+        self.b = torch.randn(*batch_dims, 1, num_channels, dtype=torch.float64) * 10
+        self.c = torch.randn(*batch_dims, 1, num_channels, dtype=torch.float64) * 10
+        self.d1 = -self.c / (3 * start)
+        self.d2 = -self.c / (3 * end)
+
+    def evaluate(self, t):
+        t = t.unsqueeze(-1)
+        t_sq = t ** 2
+        t_cu = t_sq * t
+        d = torch.where(t > 0, self.d2, self.d1)
+        return self.a + self.b * t + self.c * t_sq + d * t_cu
+
+    def derivative(self, t):
+        t = t.unsqueeze(-1)
+        t_sq = t ** 2
+        d = torch.where(t > 0, self.d2, self.d1)
+        return self.b + 2 * self.c * t + 3 * d * t_sq
+
+
+def test_interp():
+    for _ in range(10):
+        for drop in (False, True):
+            num_points = torch.randint(low=5, high=100, size=(1,)).item()
+            times1 = torch.rand(num_points // 2, dtype=torch.float64) - 1
+            times2 = torch.rand(num_points // 2, dtype=torch.float64)
+            times = torch.cat([times1, times2, torch.tensor([0.], dtype=torch.float64)]).sort().values
+            num_channels = torch.randint(low=1, high=3, size=(1,)).item()
+            num_batch_dims = torch.randint(low=0, high=3, size=(1,)).item()
+            batch_dims = []
+            for _ in range(num_batch_dims):
+                batch_dims.append(torch.randint(low=1, high=3, size=(1,)).item())
+            cubic = _Cubic(batch_dims, num_channels, start=times[0], end=times[-1])
+            values = cubic.evaluate(times)
+            if drop:
+                for values_slice in values.unbind(dim=-1):
+                    num_drop = int(num_points * torch.randint(low=1, high=4, size=(1,)).item() / 10)
+                    num_drop = min(num_drop, num_points - 4)
+                    to_drop = torch.randperm(num_points - 2)[:num_drop] + 1  # don't drop first or last
+                    values_slice[to_drop] = float('nan')
+            coeffs = torchcubicspline.natural_cubic_spline_coeffs(times, values)
+            spline = torchcubicspline.NaturalCubicSpline(coeffs)
+            _test_equal(batch_dims, num_channels, cubic, spline)
+
+# TODO: test edge cases
+
+
+def _test_equal(batch_dims, num_channels, obj1, obj2):
+    for dimension in (0, 1, 2):
+        sizes = []
+        for _ in range(dimension):
+            sizes.append(torch.randint(low=1, high=4, size=(1,)).item())
+        expected_size = tuple(batch_dims) + tuple(sizes) + (num_channels,)
+        eval_times = torch.rand(sizes, dtype=torch.float64) * 3 - 1.5
+        obj1_evaluate = obj1.evaluate(eval_times)
+        obj2_evaluate = obj2.evaluate(eval_times)
+        obj1_derivative = obj1.derivative(eval_times)
+        obj2_derivative = obj2.derivative(eval_times)
+        assert obj1_evaluate.shape == expected_size
+        assert obj2_evaluate.shape == expected_size
+        assert obj1_derivative.shape == expected_size
+        assert obj2_derivative.shape == expected_size
+        assert obj1_evaluate.allclose(obj2_evaluate)
+        assert obj1_derivative.allclose(obj2_derivative)
+
+
+def test_example1():
+    import torch
+    from torchcubicspline import (natural_cubic_spline_coeffs,
+                                  NaturalCubicSpline)
+
+    length, channels = 7, 3
+    t = torch.linspace(0, 1, length)
+    x = torch.rand(length, channels)
+    coeffs = natural_cubic_spline_coeffs(t, x)
+    spline = NaturalCubicSpline(coeffs)
+    point = torch.tensor(0.4)
+    out = spline.evaluate(point)
+    assert out.shape == (3,)
+
+
+def test_example2():
+    import torch
+    from torchcubicspline import (natural_cubic_spline_coeffs,
+                                  NaturalCubicSpline)
+
+    t = torch.linspace(0, 1, 7)
+    # (2, 1) are batch dimensions. 7 is the time dimension
+    # (of the same length as t). 3 is the channel dimension.
+    x = torch.rand(2, 1, 7, 3)
+    coeffs = natural_cubic_spline_coeffs(t, x)
+    # coeffs is a tuple of tensors
+
+    # ...at this point you can save the coeffs, put them
+    # through PyTorch's Datasets and DataLoaders, etc...
+
+    spline = NaturalCubicSpline(coeffs)
+
+    point = torch.tensor(0.4)
+    # will be a tensor of shape (2, 1, 3), corresponding to
+    # batch, batch, and channel dimensions
+    out = spline.derivative(point)
+    assert out.shape == (2, 1, 3)
+
+    point = torch.tensor([[0.4, 0.5]])
+    # will be a tensor of shape (2, 1, 1, 2, 3), corresponding to
+    # batch, batch, time, time and channel dimensions
+    out = spline.derivative(point)
+    assert out.shape == (2, 1, 1, 2, 3)
+
+
+def test_specification():
+    for _ in range(10):
+        for num_batch_dims in (0, 1, 2, 3):
+            batch_dims = []
+            for _ in range(num_batch_dims):
+                batch_dims.append(torch.randint(low=1, high=3, size=(1,)).item())
+            length = torch.randint(low=5, high=10, size=(1,)).item()
+            channels = torch.randint(low=1, high=5, size=(1,)).item()
+            t = torch.linspace(0, 1, length)
+            x = torch.rand(*batch_dims, length, channels)
+            coeffs = torchcubicspline.natural_cubic_spline_coeffs(t, x)
+            spline = torchcubicspline.NaturalCubicSpline(coeffs)
+            for i, point in enumerate(t):
+                out = spline.evaluate(point)
+                xi = x[..., i, :]
+                assert out.allclose(xi)
